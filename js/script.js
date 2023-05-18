81cbd161f0ec904fd846af95177dd1c1898958f2
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-18 17:19:00 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Colton
+// Created on: april 2023
+// This program calculate the volume of a sphere
+
+'use strict'
+/**
+* This function calculates area and perimeter of rectangle.
+*/
+function calculate() {
+ // input
+  const raduis = parseInt(document.getElementById('Raduis').value)
+
+
+
+ // process
+ const volume = (4/3 * Math.PI * Math.pow(raduis, 3)).toFixed(2)
+
+
+ // output
+  document.getElementById('volume').innerHTML =
+   'volume is: ' + volume + ' cm³'
+}
